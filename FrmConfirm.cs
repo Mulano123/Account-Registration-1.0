a9378b9ccef142ebde4accf7f556319af7d96af4
joshua <joshua@LAPTOP-K41S3KR1>
2023-09-11 14:17:57 +0800

    Add project files.

---
+++ FrmConfirm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Account_Registration
+{
+    public partial class FrmConfirm : Form
+    {
+
+        private DelagateText DelProgram, DelLastName, DelFirstName, DelMiddleName, DelAddress;
+
+
+        private DelagateNumber DelNumAge, DelNumContactNo, DelStudNo;
+        public FrmConfirm()
+        {
+            InitializeComponent();
+            DelProgram = new DelagateText(StudentInfoClass.GetProgram);
+            DelLastName = new DelagateText(StudentInfoClass.GetLastName);
+            DelFirstName = new DelagateText (StudentInfoClass.GetFirstName);
+            DelMiddleName = new DelagateText(StudentInfoClass.GetMiddleName);
+            DelAddress = new DelagateText(StudentInfoClass.GetAddress);
+
+            DelNumAge = new DelagateNumber(StudentInfoClass.GetAge);
+            DelNumContactNo = new DelagateNumber(StudentInfoClass.GetContactNo);
+            DelStudNo = new DelagateNumber(StudentInfoClass.GetStudentNo);
+        }
+
+        private void FrmConfirm_Load(object sender, EventArgs e)
+        {
+            label9.Text = DelStudNo(StudentInfoClass.StudentNo).ToString();
+            label10.Text = DelProgram(StudentInfoClass.Program);
+            label11.Text = DelLastName(StudentInfoClass.LastName);
+            label12.Text = DelFirstName(StudentInfoClass.FirstName);
+            label13.Text = DelMiddleName(StudentInfoClass.MiddleName);
+            label14.Text = DelNumAge(StudentInfoClass.Age).ToString();
+            label15.Text = DelNumContactNo(StudentInfoClass.ContactNo).ToString();
+            label16.Text = DelAddress(StudentInfoClass.Address);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void FrmConfirm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+    }
+}
---
+++ FrmRegistration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Account_Registration
+{
+    public partial class FrmRegistration : Form
+    {
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            StudentInfoClass.StudentNo = Convert.ToInt64(textBox1.Text);
+            StudentInfoClass.Program = comboBox1.Text.ToString();
+            StudentInfoClass.LastName = textBox2.Text.ToString();
+            StudentInfoClass.FirstName = textBox3.Text.ToString();
+            StudentInfoClass.MiddleName = textBox4.Text.ToString();
+            StudentInfoClass.Age = Convert.ToInt64(textBox5.Text);
+            StudentInfoClass.ContactNo = Convert.ToInt64(textBox6.Text);
+            StudentInfoClass.Address = textBox7.Text.ToString();
+
+            FrmConfirm frmC = new FrmConfirm();
+
+            if(frmC.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                textBox6.Clear();
+                textBox7.Clear();
+                comboBox1.SelectedIndex = -1;
+
+            }
+
+        }
+    }
+}
